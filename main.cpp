f1be5436699fa63c4f37a076507c95c8fc5b4622
suhyun <86099781+suhyun3579@users.noreply.github.com>
2025-03-08 22:14:19 +0900

    feat : Heap 메모리 관리
    - Console 관련 코드 부분 수정
    - FPS 표시
    - Mouse 누름 여부 Console창 출력 방식으로 변경

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@
 #include "UCamera.h"
 #include "UCubeComp.h"
 #include "ULog.h"
+#include "MemoryManager.h"
 
 extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
@@ -114,13 +115,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		sampleCube4.Render(mainCamera.viewMatrix, mainCamera.projectionMatrix);
 		sampleCube5.Render(mainCamera.viewMatrix, mainCamera.projectionMatrix);
 
-		// IMGUI Section Start
+		#pragma region ImGui
 		ImGui_ImplDX11_NewFrame();
 		ImGui_ImplWin32_NewFrame();
 		ImGui::NewFrame();
 
-		ImGui::Begin("Jungle Property Window");
-		ImGui::Text("Hello Jungle World");
+		ImGui::Begin("Jungle Control Panel");
+		ImGui::Text("Hello Jungle World!");
+		ImGui::Text("FPS %.1f (%.2f ms)", ImGui::GetIO().Framerate, 1000.0f / ImGui::GetIO().Framerate);
+		ImGui::Separator();
 
 		ImGui::Text("Keyboard State:");
 		for (const auto& [key, isPressed] : InputManager::GetInstance().GetKeyStates()) {
@@ -133,13 +136,15 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		ImGui::Separator();
 		ImGui::Text("Mouse State:");
 		ImGui::Text("Position: (%d, %d)", InputManager::GetInstance().GetMousePosition().x, InputManager::GetInstance().GetMousePosition().y);
-		ImGui::Text("Left Button: %s", InputManager::GetInstance().IsMouseButtonDown(VK_LBUTTON) ? "Pressed" : "Released");
-		ImGui::Text("Right Button: %s", InputManager::GetInstance().IsMouseButtonDown(VK_RBUTTON) ? "Pressed" : "Released");
 		ImGui::Text("camera position: %f, %f, %f", mainCamera.RelativeLocation.X, mainCamera.RelativeLocation.Y, mainCamera.RelativeLocation.Z);
 		ImGui::Text("camera up direction: %f, %f, %f", mainCamera.upDirection.X, mainCamera.upDirection.Y, mainCamera.upDirection.Z);
 		ImGui::Text("camera facing: %f, %f, %f", mainCamera.facing.X, mainCamera.facing.Y, mainCamera.facing.Z);
 		ImGui::Text("camera looking at: %f, %f, %f", mainCamera.targetPos.X, mainCamera.targetPos.Y, mainCamera.targetPos.Z);
 		ImGui::Text("view Matrix:\n%s", mainCamera.viewMatrix.PrintMatrix().c_str());
+
+		// Heap Memory
+		ImGui::Text("Total Bytes  : %d", MemoryManager::GetInstance().GetTotalAllocationBytes());
+		ImGui::Text("Total Count  : %d", MemoryManager::GetInstance().GetTotalAllocationCount());
 		ImGui::End();
 
 		// UE_LOG
@@ -153,10 +158,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 		ImGui::Render();
 		ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
-		// IMGUI Section End
+		#pragma endregion
 
 		URenderer::GetInstance().SwapBuffer();
-
 		do {
 			Sleep(0);
 			QueryPerformanceCounter(&endTime);
